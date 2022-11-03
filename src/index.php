5072c01488ecfaa72f345ecf5e527fb1212272db
Gustavo Souza <gustavosouzacco@gmail.com>
2022-11-03 00:23:48 -0300

    Criação do arquivo SMTPServer

--- src/index.php
+++ src/index.php
@@ -2,20 +2,4 @@
 
 namespace Gustavo\WorkerMailer\src;
 
-include_once "../vendor/autoload.php";
-
-use PHPMailer\PHPMailer\PHPMailer;
-
-  echo "to aqui";
-
-  $phpmailer = new PHPMailer();
-  $phpmailer->isSMTP();
-  $phpmailer->Host = 'smtp.mailtrap.io';
-  $phpmailer->SMTPAuth = true;
-  $phpmailer->Port = 2525;
-  $phpmailer->Username = 'd2800210e6ab99';
-  $phpmailer->Password = 'f597be385f5c18';
-
-  $phpmailer->createBody();
-
-  $phpmailer->send();
+require_once "./vendor/autoload.php";
